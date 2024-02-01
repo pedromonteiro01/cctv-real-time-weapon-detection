b75879d1746b3b4292eea220ee76c3bd3231364e
pedromonteiro01 <pmapm@ua.pt>
2024-02-01 11:48:05 +0000

    change frame image to actual video

--- project/frontend/src/components/ControllButton/ControllButton.js
+++ project/frontend/src/components/ControllButton/ControllButton.js
@@ -2,7 +2,7 @@ import './ControllButton.css';
 
 const ControllButton = (props) => {
     return (
-        <button className='controll-button'>
+        <button className='controll-button' onClick={props.onClick}>
             {props.icon}
         </button>
     )
--- project/frontend/src/views/Dashboard/VideoFrames/DetectionFrame.js
+++ project/frontend/src/views/Dashboard/VideoFrames/DetectionFrame.js
@@ -1,19 +1,30 @@
-// DetectionFrame.js
-import React from 'react';
+import React, { useState } from 'react';
 import './DetectionFrame.css';
-import frame from './frame.png'; // Assuming this is the background frame image
 import ControllButton from '../../../components/ControllButton/ControllButton';
-import { CgMaximizeAlt, CgController } from "react-icons/cg";
+import { CgMaximizeAlt, CgController, CgClose } from "react-icons/cg";
 import { IoCameraOutline } from "react-icons/io5";
 import { PiFilmSlateLight } from "react-icons/pi";
 import { BiCctv, BiTargetLock } from "react-icons/bi";
 import { MdOutlineCalendarToday, MdOutlineWatchLater } from "react-icons/md";
 import { BsSkipBackward } from "react-icons/bs";
+import shoot from './shoot6.mp4'
 
 const DetectionFrame = () => {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    // Handler to toggle video expansion
+    const toggleExpandVideo = () => {
+        setIsExpanded(!isExpanded);
+    };
+
     return (
-        <div className='video-frame'>
-            <img src={frame} alt='Surveillance feed' className='frame-image' />
+        <div className={`video-frame ${isExpanded ? 'expanded' : ''}`}>
+            <video src={shoot} alt='Surveillance feed' className='frame-video' loop autoPlay muted></video>
+            {isExpanded && (
+                <button className="exit-fullscreen-button" onClick={toggleExpandVideo}>
+                    <CgClose /> {/* Icon for closing or minimizing */}
+                </button>
+            )}
             <div className='overlay'>
                 <div className='camera-info'>
                     <div className='camera-info-wrapper'>
@@ -33,7 +44,7 @@ const DetectionFrame = () => {
                     </div>
                     <div className='controll-buttons-2'>
                         <ControllButton icon={<CgController />} />
-                        <ControllButton icon={<CgMaximizeAlt />} />
+                        <ControllButton icon={<CgMaximizeAlt onClick={toggleExpandVideo} />}  />
                     </div>
                 </div>
             </div>
