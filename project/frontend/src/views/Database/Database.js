003f3c582d685131e2ff81e21744f503000f3fb0
pedromonteiro01 <pmapm@ua.pt>
2024-05-22 19:13:43 +0100

    add pagination

--- project/frontend/src/views/Database/Database.js
+++ project/frontend/src/views/Database/Database.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { WebSocketContext } from '../../context/WebSocketContext/WebSocketContext';
 import './Database.css';
 import { ClipLoader } from 'react-spinners';
@@ -8,12 +8,30 @@ import IconButton from '../../components/IconButton/IconButton';
 
 const Database = () => {
     const { cameras, isLoading } = useContext(WebSocketContext);
+    const [currentPage, setCurrentPage] = useState(0);
+    const camerasPerPage = 4;
+
+    const cameraArray = Object.values(cameras);
+    const pageCount = Math.ceil(cameraArray.length / camerasPerPage);
+
+    const handlePrevious = () => {
+        setCurrentPage(prevPage => (prevPage > 0 ? prevPage - 1 : prevPage));
+    };
+
+    const handleNext = () => {
+        setCurrentPage(prevPage => (prevPage < pageCount - 1 ? prevPage + 1 : prevPage));
+    };
+
+    const displayedCameras = cameraArray.slice(
+        currentPage * camerasPerPage,
+        (currentPage + 1) * camerasPerPage
+    );
 
     return (
         <div className='database-wrapper'>
             <div className="database-buttons">
-                <IconButton icon={FaArrowLeft} label="Previous" onClick={null} position="left" />
-                <IconButton icon={FaArrowRight} label="Next" onClick={null} position="right" />
+                <IconButton icon={FaArrowLeft} label="Previous" onClick={handlePrevious} position="left" />
+                <IconButton icon={FaArrowRight} label="Next" onClick={handleNext} position="right" />
             </div>
             <div className='database-images-grid'>
                 {isLoading ? (
@@ -21,10 +39,13 @@ const Database = () => {
                         <ClipLoader color="#ffffff" />
                     </div>
                 ) : (
-                    Object.keys(cameras).length > 0 ?
-                        Object.values(cameras).map(camera => (
+                    displayedCameras.length > 0 ? (
+                        displayedCameras.map(camera => (
                             <CameraStream key={camera.id} camera={camera} frameSrc={camera.frameSrc} />
-                        )) : <div style={{ color: '#fff' }}>No camera data available.</div>
+                        ))
+                    ) : (
+                        <div style={{ color: '#fff' }}>No camera data available.</div>
+                    )
                 )}
             </div>
         </div>
